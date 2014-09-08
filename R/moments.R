530e7af44fc68b392731543452c6cde15f147ab4
hadley <h.wickham@gmail.com>
2014-09-07 19:27:43 -0500

    Add some documentation errors

--- R/moments.R
+++ R/moments.R
@@ -1,7 +1,6 @@
 #' Compute the moments of a random variable.
 #'
 #' @name moments
-#' @param x random variable to summarise
 #' @seealso \code{\link{Z}} to standardise an rv by subtracting off mean
 #'   and dividing by standard deviation
 #' @examples
--- R/ops.R
+++ R/ops.R
@@ -1,15 +1,15 @@
 # Combine two independent random variables (including the special case
-# of a random variable and a number). We basically generate the complete 
+# of a random variable and a number). We basically generate the complete
 # two way table, and then collapse down to a random variable
 combine <- function(e1, e2, fun) {
   e1 <- as.rv(e1)
   e2 <- as.rv(e2)
-  
+
   # Use outer to generate all pairwise combinations, combining using fun
   vals <- outer(e1, e2, fun)
   # Probabilities get multipled together
   probs <- outer(probs(e1), probs(e2), "*")
-  
+
   # Rely on rv to collapse any duplicates
   rv(as.vector(vals), as.vector(probs))
 }
@@ -47,9 +47,9 @@ combine <- function(e1, e2, fun) {
 
 
 #' Random if.
-#' 
+#'
 #' @param x an event (a random variable with logical values)
-#' @param yes number to return if event occurs
+#' @param yess number to return if event occurs
 #' @param no number to return if event doesn't occur
 #' @export
 #' @examples
